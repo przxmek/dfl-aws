f9c4f6277c8fca6b38f69506a9f759529b26c820
Przemek Kuczyński <przxmek@gmail.com>
2020-04-09 20:39:51 +0200

    Add files via upload

--- aws/ec2-dfl.sh
+++ aws/ec2-dfl.sh
@@ -1,25 +1,40 @@
 #!/bin/bash
 
-# Update environment
-sudo apt update
-conda update -y -n base -c defaults conda
-conda init bash
+###############################################################################
+##                                                                           ##
+## Tested on AMIs:
+##   - Deep Learning AMI (Ubuntu 18.04) Version 27.0 (ami-008d8ed4bd7dc2485)
+##                                                                           ##
+###############################################################################
 
-# Install ffmpeg
+echo ">>> Installing EFL DL stack. Sit back and relax..."
+
+## Update environment
+echo ">>> Installing ffmpeg and 7z..."
+sudo apt update
 sudo apt install -y ffmpeg p7zip-full
 
-# Install DeepFaceLab
+echo ">>> Checking anaconda..."
+source ~/anaconda3/etc/profile.d/conda.sh # conda init
+echo ">>> Checking for conda updates..."
+conda update -y -n base -c defaults conda
+
+### Install DeepFaceLab
+echo ">>> Configuring anaconda environment for DeepFaceLab..."
 conda create -y -n deepfacelab -c main python=3.6.8 cudnn=7.6.5 cudatoolkit=10.0.130
 conda activate deepfacelab
 
+echo ">>> Cloning Git repos for DeepFaceLab..."
 git clone https://github.com/lbfs/DeepFaceLab_Linux.git
 cd DeepFaceLab_Linux
 git clone https://github.com/iperov/DeepFaceLab.git
+
+echo ">>> Installing DeepFaceLab pip requirements..."
 python -m pip install -r ./DeepFaceLab/requirements-cuda.txt
 
-
-# Download CelebA Dataset
-wget https://github.com/iperov/DeepFaceLab/releases/download/06.04.2020/DeepFaceLab_NVIDIA_build_04_06_2020.exe
+### Download CelebA Dataset
+echo ">>> Installing pretrained datasets (CelebA, Quick96)..."
+wget https://github.com/iperov/DeepFaceLab/releases/download/06.04.2020/DeepFaceLab_NVIDIA_build_04_06_$7z x DeepFaceLab_NVIDIA_build_04_06_2020.exe
 7z x DeepFaceLab_NVIDIA_build_04_06_2020.exe
 rm DeepFaceLab_NVIDIA_build_04_06_2020.exe
 mv DeepFaceLab_NVIDIA/_internal/pretrain_CelebA/ .
@@ -27,7 +42,7 @@ mv DeepFaceLab_NVIDIA/_internal/pretrain_Quick96/ .
 mv DeepFaceLab_NVIDIA/workspace .
 rm -r DeepFaceLab_NVIDIA
 
-# Ready to go
+## Ready to go
 cd scripts/
 ll
 
---
+++ aws/gcp-dfl.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Update environment
+sudo apt update
+sudo apt install -y ffmpeg p7zip-full
+
+
+echo ">>> Intalling anaconda..."
+wget https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh
+chmod +x Anaconda3-2020.02-Linux-x86_64.sh
+./Anaconda3-2020.02-Linux-x86_64.sh
+rm Anaconda3-2020.02-Linux-x86_64.sh
+source ~/anaconda3/etc/profile.d/conda.sh
+
+conda init bash
+echo ">>> Checking for conda updates..."
+conda update -y -n base -c defaults conda
+
+# Install DeepFaceLab
+echo ">>> Configuring conda environment for DeepFaceLab..."
+conda create -y -n deepfacelab -c main python=3.6.8 cudnn=7.6.5 cudatoolkit=10.0.130
+source ~/anaconda3/etc/profile.d/conda.sh # conda init
+conda activate deepfacelab
+
+echo ">>> Cloning Git repos for DeepFaceLab..."
+git clone https://github.com/lbfs/DeepFaceLab_Linux.git
+cd DeepFaceLab_Linux
+git clone https://github.com/iperov/DeepFaceLab.git
+
+echo ">>> Installing DeepFaceLab pip requirements..."
+python -m pip install -r ./DeepFaceLab/requirements-cuda.txt
+
+
+# Download CelebA Dataset
+echo ">>> Installing pretrained datasets (CelebA, Quick96)..."
+wget https://github.com/iperov/DeepFaceLab/releases/download/06.04.2020/DeepFaceLab_NVIDIA_build_04_06_2020.exe
+7z x DeepFaceLab_NVIDIA_build_04_06_2020.exe
+rm DeepFaceLab_NVIDIA_build_04_06_2020.exe
+mv DeepFaceLab_NVIDIA/_internal/pretrain_CelebA/ .
+mv DeepFaceLab_NVIDIA/_internal/pretrain_Quick96/ .
+mv DeepFaceLab_NVIDIA/workspace .
+rm -r DeepFaceLab_NVIDIA
+
+# Ready to go
+cd scripts/
+ls -al
+
+echo ""
+echo ">>> Ready to go! Type 'ls -al' to see available scripts. <<<<<<<<<<<<<<<<< "
+echo ""
